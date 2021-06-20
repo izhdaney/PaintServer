4366f38081ec64850cba6356246d25151e11037e
Ilya <fmgorod@gmail.com>
2021-06-21 01:34:17 +0300

    Mock Login Controller

---
+++ PaintServer/PaintServer/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaintServer.DTO;
+using System.Net;
+
+namespace PaintServer.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class LoginController: ControllerBase
+    {
+
+            [HttpPost]
+            [Route("login")]
+            public IActionResult Login([FromBody] UserAutorizationData userAutorizationData)
+            {
+            //1  Ilya Zhdaney  zhdaney @gmail.com QWE123qazQQ
+
+            AutorizationResultData autorizationResultData = new AutorizationResultData()
+            {
+                UserID = 1,
+                FirstName = "Ilya",
+                LastName = "Zhdaney",
+                Login = "zhdaney@gmail.com",
+                AutorizationResultCode = 200
+            };
+                return Ok(autorizationResultData);
+            }
+    }
+}
--- PaintServer/PaintServer/DTO/UserAutorizationData.cs
+++ PaintServer/PaintServer/DTO/UserAutorizationData.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Team_Project_Paint.OnlinePaint.WebPaint
+namespace PaintServer.DTO
 {
     public class UserAutorizationData
     {
