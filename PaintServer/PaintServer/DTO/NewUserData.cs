8fdfb53567c2cd15f33bd1fe4e0a57481ae0d9a0
Ilya <fmgorod@gmail.com>
2021-06-20 23:45:11 +0300

    DTO Add

---
+++ PaintServer/PaintServer/DTO/NewUserData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Project_Paint.OnlinePaint.WebPaint
+{
+    public class NewUserData
+    {
+        public string Login { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+
+        public void Validate()
+        {
+            if (Login == null || Login.Length <= 1 || Login.Length > 20) throw new ArgumentException(nameof(Login));
+        }
+    }
+}
