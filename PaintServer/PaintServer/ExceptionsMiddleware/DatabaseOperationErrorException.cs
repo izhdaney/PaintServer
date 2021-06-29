7c651e04e5d916d8cea91377bad3d324633f4277
Yana Naumova <naumovayana87@gmail.com>
2021-06-29 15:07:50 +0300

    Exceptions Created

---
+++ PaintServer/PaintServer/ExceptionsMiddleware/DatabaseOperationErrorException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PaintServer.Exeptions
+{
+    public class DatabaseOperationErrorException : Exception
+    {
+        public DatabaseOperationErrorException() { }
+
+        public DatabaseOperationErrorException(string message) : base(message) { }
+    }
+}
