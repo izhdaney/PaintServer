2cb0b9a654359c05394ff5c4bb5e7afd5fe537bb
Yana Naumova <naumovayana87@gmail.com>
2021-06-29 21:41:38 +0300

    small namespace change

--- PaintServer/PaintServer/ExceptionsMiddleware/CustomExceptions/AutorizationFailException.cs
+++ PaintServer/PaintServer/ExceptionsMiddleware/CustomExceptions/AutorizationFailException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PaintServer.ExceptionsMiddleware
+namespace PaintServer.Exeptions
 {
     public class AutorizationFailException: Exception
     {
